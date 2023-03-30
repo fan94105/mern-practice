1e3b094ff7e988a3eb06d9443f9630fc3ac37e75
fan94105 <fanfan94105@gmail.com>
2023-03-31 00:05:54 +0800

    REACT_APP...

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = `${process.env.SERVER_URL}/api/courses`;
+const API_URL = `${process.env.REACT_APP_SERVER_URL}/api/courses`;
 // const API_URL = "http://localhost:8080/api/courses";
 
 class CourseService {
